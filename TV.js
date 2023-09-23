0b2199ea0fdd150eb00acc01f93bc591ea8fae9a
Ishita Sharma <110284120+IshitaSharma123@users.noreply.github.com>
2023-09-23 12:12:59 +0530

    Add files via upload

---
+++ TV.js
@@ -0,0 +1,34 @@
+Status = "";
+TV_IMG = '';
+objects = [];
+
+function preload() {
+    TV_IMG = loadImage("IMG-6964.JPG");
+}
+
+function setup() {
+    canvas = createCanvas(640, 640);
+    canvas.center();
+
+    video = createCanvas(VIDEO);
+    video.size(640, 640);
+    video.hide();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects...";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    Status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
